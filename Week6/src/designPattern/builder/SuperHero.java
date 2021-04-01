dfceeb7a8e1fed983a31bcbffe6ae2b2c4c0b62f
unknown <vladonis1234@gmail.com>
2021-04-01 15:22:17 +0300

    Week6 seminar's end

--- Week6/src/designPattern/builder/SuperHero.java
+++ Week6/src/designPattern/builder/SuperHero.java
@@ -14,12 +14,12 @@ public class SuperHero
 	private SuperPowerInferface superPower1;
 	private SuperPowerInferface superPower2;
 	
-	public SuperHero() 
+	private SuperHero() 
 	{
 		
 	}
 	
-	public SuperHero(String name, int lifePoints, boolean isVillain, boolean isWounded, 
+	private SuperHero(String name, int lifePoints, boolean isVillain, boolean isWounded, 
 			WeaponInferface rightWeapon, WeaponInferface leftWeapon,
 			SuperPowerInferface superPower1, SuperPowerInferface superPower2) 
 	{
@@ -32,6 +32,56 @@ public class SuperHero
 		this.leftWeapon = leftWeapon;
 		this.superPower1 = superPower1;
 		this.superPower2 = superPower2;
+	}
+	
+	public static class SuperHeroBuilder
+	{
+		SuperHero superHero;		
+		
+		public SuperHeroBuilder(String name , int lifepoints)
+		{
+			this.superHero=new SuperHero();
+			this.superHero.name = name;
+			this.superHero.lifePoints = lifepoints;			
+		}
+		
+		public SuperHeroBuilder isVillain() 
+		{
+			this.superHero.isVillain = true;
+			return this;
+		}
+		
+		public SuperHeroBuilder isWounder()
+		{
+			this.superHero.isWounded = true;
+			return this;			
+		}
+		public SuperHeroBuilder setRightWeapon(WeaponInferface weapon)
+		{
+			this.superHero.rightWeapon = weapon;
+			return this;
+		}
+		public SuperHeroBuilder setLeftWeapon(WeaponInferface weapon)
+		{
+			this.superHero.leftWeapon = weapon;
+			return this;
+		}
+		public SuperHeroBuilder setSuperPower1(SuperPowerInferface power1)
+		{
+			this.superHero.superPower1 = power1;
+			return this;
+		}
+		public SuperHeroBuilder setSuperPower2(SuperPowerInferface power2)
+		{
+			this.superHero.superPower2 = power2;
+			return this;
+		}
+		
+		public SuperHero build()
+		{
+			return this.superHero;
+		}
+		
 	}
 
 
--- Week6/src/designPattern/builder/TestBuilder.java
+++ Week6/src/designPattern/builder/TestBuilder.java
@@ -4,13 +4,23 @@ public class TestBuilder
 {
 	public static void main(String[] args)
 	{
-		SuperHero superHero = new SuperHero();
+		/*
+		 * SuperHero superHero = new SuperHero();
+		 * SuperHero superHero2 = new SuperHero("Superman", 100, false, false, new
+		 * Weapon(), null, new Flying(), null);
+		 */
+	
+		SuperHero supermanHero = 
+				new SuperHero.SuperHeroBuilder("Superman", 1000).build();
 		
-		SuperHero superHero2 
-		= new SuperHero("Superman", 100, 
-				false, false, 
-				new Weapon(), null, new Flying(), null);
-		
+		SuperHero joker =
+				new SuperHero.SuperHeroBuilder("Joker", 200)
+				.isVillain()
+				.isWounder()
+				.setLeftWeapon(new Weapon())
+				.build();
+				
+	
 		
 	}
 
