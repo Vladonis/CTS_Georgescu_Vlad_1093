c0b87dd15c087b9248b961ef2921b4c308daabe0
unknown <vladonis1234@gmail.com>
2021-04-15 15:29:11 +0300

    Week 8 seminar's end

---
+++ Week8/src/decorator/WoundedDecorator.java
@@ -0,0 +1,24 @@
+package decorator;
+
+import adapter.ACMECharacter;
+
+public class WoundedDecorator extends ACMEAbstractDecorator{
+	
+	public final static int MIN_POINTS = 200;
+
+	public WoundedDecorator(ACMECharacter hero) {
+		super(hero);
+	}
+
+	@Override
+	public void move() {
+		if(this.character.getLifePoints() < MIN_POINTS) {
+			System.out.println("You can't move. Is critical wounded");
+		}
+		else
+			this.character.move();
+	}
+	
+	
+
+}
